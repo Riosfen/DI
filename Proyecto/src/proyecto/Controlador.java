a48f94e9d7c3791b3a98aa4034d9fbd3be9890e2
samo_ <samo_@DESKTOP-GO6FSUJ>
2016-11-14 23:49:01 +0100

    Proyecto di

---
+++ Proyecto/src/proyecto/Controlador.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author samo_
+ */
+public class Controlador implements ActionListener {
+
+    private Vista v;
+    
+    public Controlador(Vista v){
+    
+        this.v = v;
+    
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        switch(e.getActionCommand()){
+            case "buscar":
+                String url = v.getUrl().getText();
+                v.cargarPaginaWeb(url);
+                break;
+            case "negrita":
+                v.setNegrita();
+            break;
+            case "cursiva":
+                v.setCursiva();
+            break;
+            case "subrayar":
+                v.setSubrayado();
+            break;
+            case "editar":
+                v.setEditar();
+            break;
+        }
+        
+    }
+    
+    
+    
+}
